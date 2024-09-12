c3cb42f04d065c0e481ca66ae2a3edce7c5aac74
Ivan Molodetskikh <yalterz@gmail.com>
2024-09-12 11:53:10 +0300

    Add SwitchPresetWindowHeight by id

--- niri-ipc/src/lib.rs
+++ niri-ipc/src/lib.rs
@@ -351,7 +351,13 @@ pub enum Action {
     /// Switch between preset column widths.
     SwitchPresetColumnWidth {},
     /// Switch between preset window heights.
-    SwitchPresetWindowHeight {},
+    SwitchPresetWindowHeight {
+        /// Id of the window whose height to switch.
+        ///
+        /// If `None`, uses the focused window.
+        #[cfg_attr(feature = "clap", arg(long))]
+        id: Option<u64>,
+    },
     /// Toggle the maximized state of the focused column.
     MaximizeColumn {},
     /// Change the width of the focused column.
