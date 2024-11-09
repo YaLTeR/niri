b18cfbae238d53de193fc7671f56ec779574094c
Ivan Molodetskikh <yalterz@gmail.com>
2024-11-09 17:46:23 +0300

    niri-ipc: Add README and Cargo.toml metadata

--- niri-ipc/src/lib.rs
+++ niri-ipc/src/lib.rs
@@ -19,6 +19,13 @@
 //!
 //! This crate follows the niri version. It is **not** API-stable in terms of the Rust semver. In
 //! particular, expect new struct fields and enum variants to be added in patch version bumps.
+//!
+//! Use an exact version requirement to avoid breaking changes:
+//!
+//! ```toml
+//! [dependencies]
+//! niri-ipc = "=0.1.10"
+//! ```
 #![warn(missing_docs)]
 
 use std::collections::HashMap;
