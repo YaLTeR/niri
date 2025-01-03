52a2aa0969b563a7976814dc89d822679c5929d2
YaLTeR <YaLTeR@users.noreply.github.com>
2025-01-03 14:37:57 +0000

    deploy: b3c6f0e661878c7ab4f3c84c480ae61a5de5d3b3

---
+++ trait.impl/niri_ipc/state/trait.EventStreamStatePart.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["niri_ipc",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[15]}
