732cfb3ed5120463a0a7c6e38f61a9dd06e53a41
YaLTeR <YaLTeR@users.noreply.github.com>
2025-01-21 08:42:26 +0000

    deploy: acd4cb51aa0e013bfec14444ee48a01c60ebaf8a

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["niri_ipc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"niri_ipc/state/struct.EventStreamState.html\" title=\"struct niri_ipc::state::EventStreamState\">EventStreamState</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"niri_ipc/state/struct.KeyboardLayoutsState.html\" title=\"struct niri_ipc::state::KeyboardLayoutsState\">KeyboardLayoutsState</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"niri_ipc/state/struct.WindowsState.html\" title=\"struct niri_ipc::state::WindowsState\">WindowsState</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"niri_ipc/state/struct.WorkspacesState.html\" title=\"struct niri_ipc::state::WorkspacesState\">WorkspacesState</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1219]}
