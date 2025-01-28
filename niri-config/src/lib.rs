9548fad682cca9c2eb2a49d36a3d459328219194
may <m4rch3n1ng@gmail.com>
2025-01-28 23:12:59 +0100

    add option to set xkb config from file

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -136,6 +136,8 @@ pub struct Xkb {
     pub variant: String,
     #[knuffel(child, unwrap(argument))]
     pub options: Option<String>,
+    #[knuffel(child, unwrap(argument))]
+    pub file: Option<String>,
 }
 
 impl Xkb {
