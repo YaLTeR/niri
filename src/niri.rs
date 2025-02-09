a74cc1f1e7f0f1ff48b2b7b042c9c290d23bf9c8
agausmann@fastmail.com <adam@gaussian.dev>
2025-02-09 13:47:35 -0600

    Implement the wlr-output-power-management protocol
    This protocol allows external programs (e.g. wlopm) to control and query
    the output power state (on/off).
    
    In the current implementation, when the client sends a
    `set_mode` request, the handler calls `activate_monitors()` or
    `deactivate_monitors()` to set that state for all monitors. This
    is probably simpler than adding full support for per-output power
    management, and is good enough for the most common use cases like idle
    screensavers.
    
    The `output_power.mode` event is now emitted by `activate_monitors()`
    and `deactivate_monitors()` when the state changes. A bit of a rewrite
    was needed because the tty backend tries to activate monitors on resume,
    but previously it wasn't calling `activate_monitors()` because it is
    unable to provide a reference to the `Backend` that wraps it. I worked
    around this by adding a second method that does not take a backend
    parameter, instead assuming that the backend will take care of calling
    itself.

--- src/niri.rs
+++ src/niri.rs
@@ -146,6 +146,7 @@ use crate::protocols::foreign_toplevel::{self, ForeignToplevelManagerState};
 use crate::protocols::gamma_control::GammaControlManagerState;
 use crate::protocols::mutter_x11_interop::MutterX11InteropManagerState;
 use crate::protocols::output_management::OutputManagementManagerState;
+use crate::protocols::output_power_management::{self, OutputPowerManagementManagerState};
 use crate::protocols::screencopy::{Screencopy, ScreencopyBuffer, ScreencopyManagerState};
 use crate::protocols::virtual_pointer::VirtualPointerManagerState;
 use crate::render_helpers::debug::draw_opaque_regions;
@@ -276,6 +277,7 @@ pub struct Niri {
     pub ext_workspace_state: ExtWorkspaceManagerState,
     pub screencopy_state: ScreencopyManagerState,
     pub output_management_state: OutputManagementManagerState,
+    pub output_power_management_state: OutputPowerManagementManagerState,
     pub viewporter_state: ViewporterState,
     pub xdg_foreign_state: XdgForeignState,
     pub shm_state: ShmState,
@@ -2282,6 +2284,10 @@ impl Niri {
         let mut output_management_state =
             OutputManagementManagerState::new::<State, _>(&display_handle, client_is_unrestricted);
         output_management_state.on_config_changed(config_.outputs.clone());
+        let output_power_management_state = OutputPowerManagementManagerState::new::<State, _>(
+            &display_handle,
+            client_is_unrestricted,
+        );
         let screencopy_state =
             ScreencopyManagerState::new::<State, _>(&display_handle, client_is_unrestricted);
         let viewporter_state = ViewporterState::new::<State>(&display_handle);
@@ -2466,6 +2472,7 @@ impl Niri {
             foreign_toplevel_state,
             ext_workspace_state,
             output_management_state,
+            output_power_management_state,
             screencopy_state,
             viewporter_state,
             xdg_foreign_state,
@@ -2833,6 +2840,7 @@ impl Niri {
         self.global_space.unmap_output(output);
         self.reposition_outputs(None);
         self.gamma_control_manager_state.output_removed(output);
+        self.output_power_management_state.output_removed(output);
 
         let state = self.output_state.remove(output).unwrap();
 
@@ -2955,15 +2963,41 @@ impl Niri {
 
         self.monitors_active = false;
         backend.set_monitors_active(false);
+        for output in self.global_space.outputs() {
+            self.output_power_management_state
+                .output_power_mode_changed(output, output_power_management::Mode::Off);
+        }
     }
 
     pub fn activate_monitors(&mut self, backend: &mut Backend) {
         if self.monitors_active {
             return;
         }
+        backend.set_monitors_active(true);
+        self.activate_monitors_without_backend();
+    }
+
+    /// Note: This is intended to only be directly used by the backend module,
+    /// when the backend would want to call `activate_monitors()` but is unable
+    /// to provide a reference to the whole `Backend` type.
+    ///
+    /// The backend needs to call its own `set_monitors_active(true)` before
+    /// calling this.
+    ///
+    /// One possible long-term solution: The type of the backend argument could
+    /// be turned into a trait bound, where the trait is implemented by both the
+    /// top-level `Backend` and its inner variant types.
+    pub fn activate_monitors_without_backend(&mut self) {
+        if self.monitors_active {
+            return;
+        }
 
         self.monitors_active = true;
-        backend.set_monitors_active(true);
+
+        for output in self.global_space.outputs() {
+            self.output_power_management_state
+                .output_power_mode_changed(output, output_power_management::Mode::On);
+        }
 
         self.queue_redraw_all();
     }
