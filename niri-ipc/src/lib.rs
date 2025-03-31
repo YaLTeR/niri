36489f1daae47dc81645f4f188032b7e98a1e81e
sodiboo <37938646+sodiboo@users.noreply.github.com>
2025-03-31 07:00:10 +0200

    add toggle-keyboard-shortcuts-inhibit to CLI/IPC (#1366)
    * add toggle-keyboard-shortcuts-inhibit to CLI/IPC
    
    missed it in ef8d5274b849fe0bd76ab7159765202ac98f7bbc
    or https://github.com/YaLTeR/niri/pull/630
    or 0584dd2f1e82417bdabcc0d8cb20fddc2e8cc5e7
    or whatever
    
    * Update niri-ipc/src/lib.rs
    
    ---------
    
    Co-authored-by: Ivan Molodetskikh <yalterz@gmail.com>

--- niri-ipc/src/lib.rs
+++ niri-ipc/src/lib.rs
@@ -212,6 +212,8 @@ pub enum Action {
         #[cfg_attr(feature = "clap", arg(short = 'd', long, action = clap::ArgAction::Set, default_value_t = true))]
         write_to_disk: bool,
     },
+    /// Enable or disable the keyboard shortcuts inhibitor (if any) for the focused window.
+    ToggleKeyboardShortcutsInhibit {},
     /// Close a window.
     #[cfg_attr(feature = "clap", clap(about = "Close the focused window"))]
     CloseWindow {
