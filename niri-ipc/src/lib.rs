ad490eebca6af68b6086af7c5e9205e0c35b103f
Ivan Molodetskikh <yalterz@gmail.com>
2025-03-31 07:50:30 +0300

    Update niri-ipc/src/lib.rs

--- niri-ipc/src/lib.rs
+++ niri-ipc/src/lib.rs
@@ -212,7 +212,7 @@ pub enum Action {
         #[cfg_attr(feature = "clap", arg(short = 'd', long, action = clap::ArgAction::Set, default_value_t = true))]
         write_to_disk: bool,
     },
-    /// Enables or disables the keyboard shortcut inhibitor (if any) for the focused surface.
+    /// Enable or disable the keyboard shortcuts inhibitor (if any) for the focused window.
     ToggleKeyboardShortcutsInhibit {},
     /// Close a window.
     #[cfg_attr(feature = "clap", clap(about = "Close the focused window"))]
