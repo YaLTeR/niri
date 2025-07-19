ae0f361a93982313fe8c6ffdaef2c35d64660eef
Kent D <lolexplode@gmail.com>
2025-07-19 09:46:35 +0200

    remove copyright, this is my code after all

--- docs/hooks/remove-must-fail.py
+++ docs/hooks/remove-must-fail.py
@@ -1,27 +1,15 @@
-# Copyright (c) 2016-2025 Martin Donath <martin.donath@squidfunk.com>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-
 from __future__ import annotations
 import re
-from re import Match
 
+# todo: this could be done generically, so that any 
+# ```language,annotation,anything-else 
+# is reduced to
+# ```language
+# which is what's supported by mkdocs/pygments
+# also note: mkdocs provides ways to highlight lines, add line numbers
+# but these are added as 
+# ```language linenums="1"
+# and not split by comma
 def on_page_markdown(
     markdown: str, *, page, config, files
 ):
