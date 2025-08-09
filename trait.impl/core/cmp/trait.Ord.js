1834f6230c95fe58f5e87fa15aea9d198c7caffa
YaLTeR <YaLTeR@users.noreply.github.com>
2025-08-09 12:31:01 +0000

    deploy: 67361f88fd01974ebee4cf80f0e29c87d805cc39

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["niri_ipc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"niri_ipc/enum.Layer.html\" title=\"enum niri_ipc::Layer\">Layer</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[244]}
