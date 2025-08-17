7d1e3dd4870d758fcefff059ea97f3d3126636dc
Ivan Molodetskikh <yalterz@gmail.com>
2025-08-17 10:01:50 +0300

    fixes

--- src/main.rs
+++ src/main.rs
@@ -38,25 +38,33 @@ const DEFAULT_LOG_FILTER: &str = "niri=debug,smithay::backend::renderer::gles=er
 static GLOBAL: tracy_client::ProfiledAllocator<std::alloc::System> =
     tracy_client::ProfiledAllocator::new(std::alloc::System, 100);
 
-// SCHED_RESET_ON_FORK only exists on linux
+// SCHED_RESET_ON_FORK only exists on Linux.
 #[cfg(target_os = "linux")]
-fn set_rt_scheduling() -> () {
-    // Set SCHED_RESET_ON_FORK and request minimal realtime round-robin prio for main pid
-    let param = libc::sched_param {
-        sched_priority: unsafe { libc::sched_get_priority_min(libc::SCHED_RR) }
+fn set_rt_scheduling() {
+    let res = unsafe {
+        // Work around libc crate exposing more fields on musl.
+        let mut param: libc::sched_param = std::mem::zeroed();
+        // Set SCHED_RESET_ON_FORK and request minimal realtime round-robin prio for main pid.
+        param.sched_priority = libc::sched_get_priority_min(libc::SCHED_RR);
+
+        libc::pthread_setschedparam(
+            libc::pthread_self(),
+            libc::SCHED_RR | libc::SCHED_RESET_ON_FORK,
+            &param,
+        )
     };
-    let res = unsafe { libc::pthread_setschedparam(libc::pthread_self(), libc::SCHED_RR | libc::SCHED_RESET_ON_FORK, &param) };
+
     match res {
-        libc::EPERM => debug!("No permission to set real-time policy"),
-        libc::EINVAL => debug!("RT Policy not recognized or scheduling params wrong"),
-        libc::ESRCH => debug!("Thread ID not found for RT scheduling policy"),
+        libc::EPERM => debug!("no permission to set real-time policy"),
+        libc::EINVAL => debug!("real-time policy not recognized or scheduling params wrong"),
+        libc::ESRCH => debug!("thread ID not found for real-time policy"),
         0 => (),
-        _ => warn!("Unknown failure: {res}")
+        _ => warn!("unknown failure setting real-time policy: {res}"),
     }
 }
 
 #[cfg(not(target_os = "linux"))]
-fn set_rt_scheduling() -> () {
+fn set_rt_scheduling() {
     ()
 }
 
