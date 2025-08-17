42eb32c9c079f3be09368d4b2e4d2c876f8d6924
Ivan Molodetskikh <yalterz@gmail.com>
2025-08-17 10:03:55 +0300

    move to utils

--- src/main.rs
+++ src/main.rs
@@ -38,36 +38,6 @@ const DEFAULT_LOG_FILTER: &str = "niri=debug,smithay::backend::renderer::gles=er
 static GLOBAL: tracy_client::ProfiledAllocator<std::alloc::System> =
     tracy_client::ProfiledAllocator::new(std::alloc::System, 100);
 
-// SCHED_RESET_ON_FORK only exists on Linux.
-#[cfg(target_os = "linux")]
-fn set_rt_scheduling() {
-    let res = unsafe {
-        // Work around libc crate exposing more fields on musl.
-        let mut param: libc::sched_param = std::mem::zeroed();
-        // Set SCHED_RESET_ON_FORK and request minimal realtime round-robin prio for main pid.
-        param.sched_priority = libc::sched_get_priority_min(libc::SCHED_RR);
-
-        libc::pthread_setschedparam(
-            libc::pthread_self(),
-            libc::SCHED_RR | libc::SCHED_RESET_ON_FORK,
-            &param,
-        )
-    };
-
-    match res {
-        libc::EPERM => debug!("no permission to set real-time policy"),
-        libc::EINVAL => debug!("real-time policy not recognized or scheduling params wrong"),
-        libc::ESRCH => debug!("thread ID not found for real-time policy"),
-        0 => (),
-        _ => warn!("unknown failure setting real-time policy: {res}"),
-    }
-}
-
-#[cfg(not(target_os = "linux"))]
-fn set_rt_scheduling() {
-    ()
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set backtrace defaults if not set.
     if env::var_os("RUST_BACKTRACE").is_none() {
@@ -276,7 +246,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         state.niri.config_error_notification.show_created(path);
     }
 
-    set_rt_scheduling();
+    niri::utils::realtime::set_rt_scheduling();
 
     // Run the compositor.
     event_loop
