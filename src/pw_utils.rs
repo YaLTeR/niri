bf4bf7a3a9e0fe11a195297680cbdbe3cdbf2b94
wrvsrx <wrvsrx@outlook.com>
2025-09-02 05:52:25 +0800

    pw_utils: rename `mark_buffer_as_good` to `mark_buffer_after_render`
    I will change semantic of this function in later commit.

--- src/pw_utils.rs
+++ src/pw_utils.rs
@@ -1253,7 +1253,7 @@ impl Cast {
                             elements.iter().rev(),
                         ) {
                             Ok(sync_point) => {
-                                mark_buffer_as_good(pw_buffer, &mut self.sequence_counter);
+                                mark_buffer_after_render(pw_buffer, &mut self.sequence_counter);
                                 trace!("queueing buffer with seq={}", self.sequence_counter);
                                 self.queue_after_sync(pw_buffer, sync_point);
                                 true
@@ -1290,7 +1290,7 @@ impl Cast {
                             elements,
                         ) {
                             Ok(()) => {
-                                mark_buffer_as_good(pw_buffer, &mut self.sequence_counter);
+                                mark_buffer_after_render(pw_buffer, &mut self.sequence_counter);
                                 trace!("queueing buffer with seq={}", self.sequence_counter);
                                 self.queue_after_sync(pw_buffer, SyncPoint::signaled());
                                 true
@@ -1336,7 +1336,7 @@ impl Cast {
 
                 match clear_dmabuf(renderer, dmabuf) {
                     Ok(sync_point) => {
-                        mark_buffer_as_good(pw_buffer, &mut self.sequence_counter);
+                        mark_buffer_after_render(pw_buffer, &mut self.sequence_counter);
                         trace!("queueing clear buffer with seq={}", self.sequence_counter);
                         self.queue_after_sync(pw_buffer, sync_point);
                         true
@@ -1362,7 +1362,7 @@ impl Cast {
 
                 match clear_shmbuf(&shmbuf) {
                     Ok (()) => {
-                        mark_buffer_as_good(pw_buffer, &mut self.sequence_counter);
+                        mark_buffer_after_render(pw_buffer, &mut self.sequence_counter);
                         trace!("queueing clear buffer with seq={}", self.sequence_counter);
                         self.queue_after_sync(pw_buffer, SyncPoint::signaled());
                         true
@@ -1522,7 +1522,7 @@ unsafe fn return_unused_buffer(stream: &Stream, pw_buffer: NonNull<pw_buffer>) {
     pw_stream_queue_buffer(stream.as_raw_ptr(), pw_buffer);
 }
 
-unsafe fn mark_buffer_as_good(pw_buffer: NonNull<pw_buffer>, sequence: &mut u64) {
+unsafe fn mark_buffer_after_render(pw_buffer: NonNull<pw_buffer>, sequence: &mut u64) {
     let pw_buffer = pw_buffer.as_ptr();
     let spa_buffer = (*pw_buffer).buffer;
     let chunk = (*(*spa_buffer).datas).chunk;
