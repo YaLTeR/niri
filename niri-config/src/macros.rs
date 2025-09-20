54d07eaa485cd3ca6f0a7cd955882af1216431b1
Ivan Molodetskikh <yalterz@gmail.com>
2025-09-20 12:57:40 +0300

    config: Replace resolve_against with MergeWith

--- niri-config/src/macros.rs
+++ niri-config/src/macros.rs
@@ -1,8 +1,32 @@
+macro_rules! merge_clone {
+    (($self:expr, $part:expr), $($field:ident),+ $(,)*) => {
+        $(
+            if let Some(x) = &$part.$field {
+                $self.$field.clone_from(x);
+            }
+        )+
+    };
+}
+
 macro_rules! merge_clone_opt {
     (($self:expr, $part:expr), $($field:ident),+ $(,)*) => {
         $(
             if $part.$field.is_some() {
                 $self.$field.clone_from(&$part.$field);
+            }
+        )+
+    };
+}
+
+macro_rules! merge_color_gradient {
+    (($self:expr, $part:expr), $(($color:ident, $gradient:ident)),+ $(,)*) => {
+        $(
+            if let Some(x) = $part.$color {
+                $self.$color = x;
+                $self.$gradient = None;
+            }
+            if let Some(x) = $part.$gradient {
+                $self.$gradient = Some(x);
             }
         )+
     };
