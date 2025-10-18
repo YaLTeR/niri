45bfa12e03c852f01cc17be8f598e1b879a6bc10
Lin Xianyi <iynaix@gmail.com>
2025-10-18 16:41:38 +0800

    Add --path argument for niri msg screenshot* commands

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -212,8 +212,11 @@ pub fn expand_home(path: &Path) -> anyhow::Result<Option<PathBuf>> {
     }
 }
 
-pub fn make_screenshot_path(config: &Config) -> anyhow::Result<Option<PathBuf>> {
-    let Some(path) = &config.screenshot_path.0 else {
+pub fn make_screenshot_path(
+    path: Option<String>,
+    config: &Config,
+) -> anyhow::Result<Option<PathBuf>> {
+    let Some(path) = path.or_else(|| config.screenshot_path.0.clone()) else {
         return Ok(None);
     };
 
