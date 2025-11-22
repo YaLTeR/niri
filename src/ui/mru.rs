e107fddaa44fa0b424ee0bf1f23cd8281245aa19
Ivan Molodetskikh <yalterz@gmail.com>
2025-11-22 08:25:36 +0300

    mru: Add Space hardcoded bind to confirm
    Expected for buttons.

--- src/ui/mru.rs
+++ src/ui/mru.rs
@@ -1840,6 +1840,7 @@ fn make_preset_opened_binds() -> Vec<Bind> {
 
     push(Keysym::Escape, Action::MruCancel);
     push(Keysym::Return, Action::MruConfirm);
+    push(Keysym::space, Action::MruConfirm);
     push(Keysym::a, Action::MruSetScope(MruScope::All));
     push(Keysym::o, Action::MruSetScope(MruScope::Output));
     push(Keysym::w, Action::MruSetScope(MruScope::Workspace));
