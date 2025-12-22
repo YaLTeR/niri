9e73beb165e51dcb8f1c0168e355baed0d9bc85c
Ivan Molodetskikh <yalterz@gmail.com>
2025-12-22 09:07:57 +0300

    shader: Store uniforms in Rc instead of Vec
    It's frequently cloned (e.g. every border piece every render) and we
    don't change it.

--- src/layout/opening_window.rs
+++ src/layout/opening_window.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use anyhow::Context as _;
 use glam::{Mat3, Vec2};
@@ -104,14 +105,14 @@ impl OpenAnimation {
                 None,
                 scale.x as f32,
                 alpha,
-                vec![
+                Rc::new([
                     mat3_uniform("niri_input_to_geo", input_to_geo),
                     Uniform::new("niri_geo_size", geo_size.to_array()),
                     mat3_uniform("niri_geo_to_tex", geo_to_tex),
                     Uniform::new("niri_progress", progress as f32),
                     Uniform::new("niri_clamped_progress", clamped_progress as f32),
                     Uniform::new("niri_random_seed", self.random_seed),
-                ],
+                ]),
                 HashMap::from([(String::from("niri_tex"), texture.clone())]),
                 Kind::Unspecified,
             )
--- src/render_helpers/border.rs
+++ src/render_helpers/border.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
 use glam::{Mat3, Vec2};
 use niri_config::{
@@ -197,7 +198,7 @@ impl BorderRenderElement {
             None,
             scale,
             alpha,
-            vec![
+            Rc::new([
                 Uniform::new("colorspace", colorspace),
                 Uniform::new("hue_interpolation", hue_interpolation),
                 Uniform::new("color_from", color_from.to_array_unpremul()),
@@ -209,7 +210,7 @@ impl BorderRenderElement {
                 Uniform::new("geo_size", geo_size.to_array()),
                 Uniform::new("outer_radius", <[f32; 4]>::from(corner_radius)),
                 Uniform::new("border_width", border_width),
-            ],
+            ]),
             HashMap::new(),
         );
     }
