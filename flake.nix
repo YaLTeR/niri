bb15da359e273f12a8957fc5f077343fa4ab8152
LuckShiba <luckshiba@protonmail.com>
2026-01-03 14:24:39 -0300

    nix: fix path replace in SystemD service

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
           postPatch = ''
             patchShebangs resources/niri-session
             substituteInPlace resources/niri.service \
-              --replace-fail '/usr/bin' "$out/bin"
+              --replace-fail 'ExecStart=niri' "ExecStart=$out/bin/niri"
           '';
 
           cargoLock = {
